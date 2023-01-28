69eed54f36574096244afcee782db4db8c80e38f
ViliIlieva <levi@abv.bg>
2023-01-28 18:48:29 +0200

    init 28.01.23

---
+++ MobiLeLeLe2/src/main/java/com/example/mobilelele2/demo/StateController.java
@@ -0,0 +1,9 @@
+package com.example.mobilelele2.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class StateController {
+}
