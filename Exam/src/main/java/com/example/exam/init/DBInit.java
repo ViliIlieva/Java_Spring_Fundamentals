7b8b67099f082d59467ad07477bb80c259dd2980
Velislava <100292906+ViliIlieva@users.noreply.github.com>
2023-02-20 14:19:07 +0200

    Update DBInit.java

--- Exam/src/main/java/com/example/exam/init/DBInit.java
+++ Exam/src/main/java/com/example/exam/init/DBInit.java
@@ -6,7 +6,6 @@ import com.example.exam.repository.ConditionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
 import java.util.Arrays;
 
 @Component
